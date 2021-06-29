735afcba3cbdff357542b252b8fd87d857204d0b
Erico Alexandre <ericoabs25@gmail.com>
2021-06-29 02:38:21 -0300

    Initial Commit

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,13 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+    <div className="App">
+      <Styled.Container>
+        <h1>Hello World</h1>
+      </Styled.Container>
+    </div>
+  );
+}
+
+export default Home;
