9838b37c73986c53a5c0a2c8261c297c95a5f574
Erico Alexandre <ericoabs25@gmail.com>
2021-06-30 04:14:32 -0300

    Definição de tema global

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,11 +1,16 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, light = false }) => {
-  return <Styled.Container light={light}>{children}</Styled.Container>;
+export const Heading = ({ children, colorDark = true, as = 'h1' }) => {
+  return (
+    <Styled.Container colorDark={colorDark} as={as}>
+      {children}
+    </Styled.Container>
+  );
 };
 
 Heading.propTypes = {
   children: P.node.isRequired,
-  light: P.bool.isRequired,
+  colorDark: P.bool.isRequired,
+  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
 };
