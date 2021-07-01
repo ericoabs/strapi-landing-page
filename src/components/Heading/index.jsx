49f045c929ddbdf7dc642f0dbede73f74ecfb064
Erico Alexandre <ericoabs25@gmail.com>
2021-07-01 03:14:30 -0300

    Estilização do componente Heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,16 +1,29 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, colorDark = true, as = 'h1' }) => {
+export const Heading = ({
+  children,
+  colorDark = true,
+  as = 'h1',
+  size = 'huge',
+  uppercase = false,
+}) => {
   return (
-    <Styled.Container colorDark={colorDark} as={as}>
+    <Styled.Title
+      colorDark={colorDark}
+      as={as}
+      size={size}
+      uppercase={uppercase}
+    >
       {children}
-    </Styled.Container>
+    </Styled.Title>
   );
 };
 
 Heading.propTypes = {
   children: P.node.isRequired,
-  colorDark: P.bool.isRequired,
+  colorDark: P.bool,
+  uppercase: P.bool,
   as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: P.oneOf(['small', 'medium', 'big', 'huge']).isRequired,
 };
