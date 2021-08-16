2fd33b3e415c2ad4598a87efaefaabe9877a2d84
Erico Alexandre <ericoabs25@gmail.com>
2021-08-16 17:11:52 -0300

    Criação do componente GridText

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,41 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+
+export const GridText = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading size="huge" uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((element) => (
+            <Styled.GridElement key={element.title}>
+              <Heading size="medium" colorDark={!background}>
+                {element.title}
+              </Heading>
+              <TextComponent>{element.description}</TextComponent>
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  background: P.bool,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }),
+  ).isRequired,
+};
