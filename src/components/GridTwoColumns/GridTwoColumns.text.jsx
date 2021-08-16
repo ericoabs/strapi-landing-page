8014d0a7128d0ab98656093afee585ac90eefaf7
Erico Alexandre <ericoabs25@gmail.com>
2021-08-16 10:45:17 -0300

    Teste de componente GridTwoColumns

--- src/components/GridTwoColumns/GridTwoColumns.text.jsx
+++ src/components/GridTwoColumns/GridTwoColumns.text.jsx
@@ -2,9 +2,11 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { GridTwoColumns } from '.';
 
+import mock from './mock';
+
 describe('<GridTwoColumns />', () => {
-  it('should render', () => {
-    renderTheme(<GridTwoColumns>Children</GridTwoColumns>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render two columns grid', () => {
+    const { container } = renderTheme(<GridTwoColumns {...mock} />);
+    expect(container).toMatchSnapshot();
   });
 });
