0845f8f1a9bd3b0efc0787b0153ed26003c082b9
Erico Alexandre <ericoabs25@gmail.com>
2021-08-16 17:45:17 -0300

    Teste de componentes

--- src/components/GridContent/GridContent.test.jsx
+++ src/components/GridContent/GridContent.test.jsx
@@ -5,8 +5,14 @@ import { GridContent } from '.';
 import mock from './mock';
 
 describe('<GridContent />', () => {
-  it('should render', () => {
+  it('should render with background', () => {
     const { container } = renderTheme(<GridContent {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+  it('should render without background', () => {
+    const { container } = renderTheme(
+      <GridContent {...mock} background={undefined} />,
+    );
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/GridTwoColumns/GridTwoColumns.text.jsx
+++ src/components/GridTwoColumns/GridTwoColumns.text.jsx
@@ -5,10 +5,14 @@ import { GridTwoColumns } from '.';
 import mock from './mock';
 
 describe('<GridTwoColumns />', () => {
-  it('should render two columns grid', () => {
+  it('should render two columns grid without background', () => {
     const { container } = renderTheme(
       <GridTwoColumns {...mock} background={undefined} />,
     );
+    expect(container).toMatchSnapshot();
+  });
+  it('should render two columns grid with background', () => {
+    const { container } = renderTheme(<GridTwoColumns {...mock} />);
     expect(container).toMatchSnapshot();
   });
 });
