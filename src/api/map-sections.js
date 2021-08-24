ee17e7cd31ca7fe35e79cee0efe98be9c919bba4
Erico Alexandre <ericoabs25@gmail.com>
2021-08-24 06:30:13 -0300

    Mapeamento de dados e testes da Section Two Columns

--- src/api/map-sections.js
+++ src/api/map-sections.js
@@ -13,8 +13,37 @@ export const mapSections = (sections = []) => {
   });
 };
 
-export const mapSectionTwoColumns = (section) => {
-  return section;
+export const mapSectionTwoColumns = (section = {}) => {
+  /*
+  {
+        "__component": "section.section-two-columns",
+        "title": "january brings us firefox 85",
+        "description": "To wrap up January, we are proud to bring you the release of Firefox 85. In this version we are bringing you support for the :focus-visible pseudo-class in CSS and associated devtools, and the complete removal of Flash support from Firefox.",
+        "metadata": {
+          "background": true,
+          "section_id": "home",
+        },
+        "image": {
+          "url": "https://res.cloudinary.com/dvawcyisr/image/upload/v1624220407/javascript_6992900498.svg",
+
+      }
+*/
+  const {
+    __component: component = '',
+    title = '',
+    description: text = '',
+    image: { url: srcImg = '' } = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+
+  return {
+    component,
+    title,
+    text,
+    srcImg,
+    background,
+    sectionId,
+  };
 };
 
 export const mapSectionContent = (section) => {
--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -1,3 +1,39 @@
+import { mapSections, mapSectionTwoColumns } from './map-sections';
+
 describe('map-sections', () => {
-  it('should map sections', () => {});
+  it('should render predefined section if no data is sent', () => {
+    const data = mapSections();
+    expect(data).toEqual([]);
+  });
+
+  it('should map section two columns', () => {
+    const data = mapSectionTwoColumns();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section two columns', () => {
+    const data = mapSectionTwoColumns({
+      __component: 'section.section-two-columns',
+      title: 'january brings us firefox 85',
+      description: 'abc',
+      metadata: {
+        background: true,
+        section_id: 'home',
+      },
+      image: {
+        url: 'a.svg',
+      },
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('home');
+    expect(data.srcImg).toBe('a.svg');
+    expect(data.text).toBe('abc');
+    expect(data.title).toBe('january brings us firefox 85');
+  });
 });
