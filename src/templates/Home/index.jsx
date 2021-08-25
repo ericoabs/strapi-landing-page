fd5bea90197141a06159828a84c87e1134a0dd55
Erico Alexandre <ericoabs25@gmail.com>
2021-08-25 10:06:08 -0300

    Carregando os dados da API no front

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,13 +1,19 @@
 import * as Styled from './styles';
-
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { mapData } from '../../api/map-data';
-import { useState } from 'react';
-import { PageNotFound } from '../PageNotFound';
+
+import { Base } from '../Base';
+
+import { mockBase } from '../Base/mock';
+
 import { Loading } from '../Loading';
+import { PageNotFound } from '../PageNotFound';
+
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+import { GridContent } from '../../components/GridContent';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -37,7 +43,37 @@ function Home() {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumns key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
