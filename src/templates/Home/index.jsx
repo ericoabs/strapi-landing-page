4ac56c820448ec0bf5d83947e836365e78adee40
Erico Alexandre <ericoabs25@gmail.com>
2021-08-25 05:27:42 -0300

    Busca dos dados e componente PageNotFound

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,42 @@
+import * as Styled from './styles';
+
+import { Base } from '../Base';
+import { mockBase } from '../Base/mock';
+import { useEffect } from 'react';
+
+import { mapData } from '../../api/map-data';
+import { useState } from 'react';
+import { PageNotFound } from '../PageNotFound';
+
+function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-page',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+        setData(pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return <Base {...mockBase} />;
+}
+
+export default Home;
