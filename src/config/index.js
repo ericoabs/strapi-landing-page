58862d8ad851927439fea660382d1fcc24d5cb26
Erico Alexandre <ericoabs25@gmail.com>
2021-08-25 11:12:50 -0300

    Adicionando títulos dinâmicos

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+export default {
+  url: 'http://localhost:1337/pages/?slug=',
+  siteName: 'Erico Alexandre',
+  defaultSlug: 'landing-page',
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -7,7 +7,7 @@ import { mapData } from '../../api/map-data';
 
 import { Base } from '../Base';
 
-import { mockBase } from '../Base/mock';
+import config from '../../config';
 
 import { Loading } from '../Loading';
 import { PageNotFound } from '../PageNotFound';
@@ -23,11 +23,11 @@ function Home() {
 
   useEffect(() => {
     const pathName = location.pathname.replace(/[^a-z0-9-_]/gi, '');
-    const slug = pathName ? pathName : 'landing-page';
+    const slug = pathName ? pathName : config.defaultSlug;
 
     const load = async () => {
       try {
-        const data = await fetch('http://localhost:1337/pages/?slug=' + slug);
+        const data = await fetch(config.url + slug);
         const json = await data.json();
         const pageData = mapData(json);
         setData(pageData[0]);
@@ -39,6 +39,20 @@ function Home() {
     load();
   }, [location]);
 
+  useEffect(() => {
+    if (data === undefined) {
+      document.title = `Página não encontrada | ${config.siteName}`;
+    }
+
+    if (data && !data.slug) {
+      document.title = `Carregando...| ${config.siteName}`;
+    }
+
+    if (data && data.title) {
+      document.title = `${data.title} | ${config.siteName}`;
+    }
+  }, [data]);
+
   if (data === undefined) {
     return <PageNotFound />;
   }
@@ -49,9 +63,6 @@ function Home() {
 
   const { menu, sections, footerHtml, slug } = data;
   const { links, text, link, srcImg } = menu;
-
-  console.log(srcImg);
-  console.log(text);
 
   return (
     <Base
